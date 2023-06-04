7692bc9bc5a83fa19bfe73b1aab9fffa3fb4f287
Gabriel <newaydsg@gmail.com>
2023-06-04 14:14:02 -0400

    Fix routes

--- api/index.js
+++ api/index.js
@@ -4,23 +4,26 @@ import "dotenv/config";
 import UserRoute from "./routes/UserRoutes.js";
 import BillRoute from "./routes/billsRoute.js";
 import connectDB from "./config/db.js";
+import morgan from "morgan";
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(cors({
     origin: "http://localhost:5173",
 }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send({ message: "API IS RUNNING" });
 });
 
 
-app.use("/auth", UserRoute);
-app.use('/', BillRoute)
+app.use("/api", UserRoute);
+app.use('/api', BillRoute)
 
 
 connectDB().then(() => {
