4b79e442e6ffe1aab787433890fef2927ec13ba5
Gabriel <newaydsg@gmail.com>
2023-06-04 01:56:12 -0400

    Deploy

--- src/components/LandingLogin.jsx
+++ src/components/LandingLogin.jsx
@@ -4,7 +4,7 @@ function LandingLogin() {
     <div className="flex flex-col justify-center items-center gap-5">
         <h1 className="text-7xl font-bold w-[550px] text-center text-slate-100" >Manage your expenses</h1>
         <p className="w-[450px] text-center text-white/80 leading-tight">Our expense tracking app helps you manage your finances easily and securely. Record your income and expenses, set budgets, and generate clear reports.</p>
-        <Link className="bg-white px-10 py-1 rounded-md text-black/90 hover:bg-slate-300 font-semibold border-none"> 
+        <Link to='/auth' className="bg-white px-10 py-1 rounded-md text-black/90 hover:bg-slate-300 font-semibold border-none"> 
             Get Started
         </Link>
     </div>
